5873b1310d3e3498e95c4247f1efdb49120462d3
senorL <2870510275@qq.com>
2026-02-21 00:12:22 +0800

    feat: Request & Form

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+var urlSwitch = make(map[string]string)
+var count = 0
 
 func main() {
 
-	http.Handle("/bili", http.RedirectHandler("https://www.bilibili.com", http.StatusFound))
-	http.Handle("/github", http.RedirectHandler("https://www.github.com", http.StatusFound))
+	http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+		url := r.FormValue("url")
+
+		count++
+		shortCode := fmt.Sprintf("%d", count)
+
+		urlSwitch[shortCode] = url
+		fmt.Fprintf(w, "生成成功！你的短链接是: http://localhost:8080/%s\n", shortCode)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		urlPath := r.URL.Path
+
+		if urlPath == "/" {
+			http.ServeFile(w, r, "index.html")
+		} else {
+			shortCode := urlPath[1:]
+
+			newUrl, ok := urlSwitch[shortCode]
+			if ok {
+				http.Redirect(w, r, newUrl, http.StatusFound)
+			} else {
+				http.NotFound(w, r)
+			}
+		}
+
+	})
 
 	http.ListenAndServe(":8080", nil)
 }
