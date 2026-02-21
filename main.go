3c67026ac930cc5cd3f757a83ac5538c3e1a3ad2
senorL <2870510275@qq.com>
2026-02-21 23:05:34 +0800

    feat: template(action, pipe...)

--- main.go
+++ main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 var urlSwitch = make(map[string]string)
 var count = 0
 
+type ResultData struct {
+	ShortCode string
+	Original  string
+}
+
 func main() {
 
 	http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
 		url := r.FormValue("url")
-
 		count++
-		shortCode := fmt.Sprintf("%d", count)
 
+		shortCode := fmt.Sprintf("%d", count)
 		urlSwitch[shortCode] = url
-		fmt.Fprintf(w, "生成成功！你的短链接是: http://localhost:8080/%s\n", shortCode)
+		t, _ := template.ParseFiles("success.html")
+
+		data := ResultData{
+			ShortCode: shortCode,
+			Original:  url,
+		}
+		t.Execute(w, data)
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		urlPath := r.URL.Path
 
 		if urlPath == "/" {
-			http.ServeFile(w, r, "index.html")
+			funcMap := template.FuncMap{"turl": TruncateURL}
+			t, _ := template.New("index.html").Funcs(funcMap).ParseFiles("index.html")
+			t.Execute(w, urlSwitch)
 		} else {
 			shortCode := urlPath[1:]
 
@@ -39,4 +52,11 @@ func main() {
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func TruncateURL(url string) string {
+	if len(url) > 30 {
+		url = url[:30] + "..."
+	}
+	return url
 }
