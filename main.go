ad19239d901de310b87086a6f35033abba584ec6
senorL <2870510275@qq.com>
2026-02-23 22:27:55 +0800

    refactor: Gorm

--- main.go
+++ main.go
@@ -7,13 +7,28 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-var urlSwitch = make(map[string]string)
+// var urlSwitch = make(map[string]string)
 var count = 0
+
+type UrlRecord struct {
+	gorm.Model
+	OriginalUrl string
+	ShortCode   string `gorm:"unique;not null"`
+}
 
 func main() {
 	r := gin.Default()
+
+	db, err := gorm.Open(sqlite.Open("short_url.db"), &gorm.Config{})
+	if err != nil {
+		panic("连接数据库失败")
+	}
+
+	db.AutoMigrate(&UrlRecord{})
 
 	r.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "页面迷路啦～")
@@ -28,26 +43,25 @@ func main() {
 	r.LoadHTMLGlob("*.html")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", urlSwitch)
+		var urlRecords []UrlRecord
+		db.Find(&urlRecords)
+		c.HTML(http.StatusOK, "index.html", urlRecords)
 	})
 	r.POST("/shorten", func(c *gin.Context) {
 		url := c.PostForm("url")
 		count++
 		shortcode := fmt.Sprintf("%d", count)
-		urlSwitch[shortcode] = url
+		urlRecord := UrlRecord{OriginalUrl: url, ShortCode: shortcode}
+		db.Create(&urlRecord)
 		c.HTML(http.StatusOK, "success.html", shortcode)
 
 	})
 	r.GET("/:shortcode", func(c *gin.Context) {
 		// shortcode 转换成 url
 		shortcode := c.Param("shortcode")
-		url, ok := urlSwitch[shortcode]
-		if ok {
-			c.Redirect(http.StatusFound, url)
-		} else {
-			c.String(http.StatusNotFound, "链接不存在")
-		}
-
+		var urlRecord UrlRecord
+		db.Where("short_code = ?", shortcode).First(&urlRecord)
+		c.Redirect(http.StatusFound, urlRecord.OriginalUrl)
 	})
 
 	r.Run()
